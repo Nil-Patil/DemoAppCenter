6c9c528c054bb07309a6bbc00b6ccdf8fee985b1
nilesh <nilesh.p@boppotechnologies.com>
2020-08-28 12:24:44 +0530

    added button and click listeners

--- app/src/main/java/com/demoappcenter/MainActivity.kt
+++ app/src/main/java/com/demoappcenter/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.demoappcenter
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(
+            application, "9d1a16fe-7730-46bc-8f0c-42cbe5fe2964",
+            Analytics::class.java, Crashes::class.java
+        )
+        button.setOnClickListener {
+            startActivity(Intent(this, MainActivity2::class.java))
+        }
     }
 }
